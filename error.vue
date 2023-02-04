2625dba3f1cf4f62651b17e5938486b277b239fd
derrick <derrickmbarani@gmail.com>
2023-02-04 21:02:14 +0300

    move error page to root of the project to work; style the error page to look good.

---
+++ error.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="h-screen flex items-center bg-[#eff1f3]">
+        <div class="card mx-auto bg-[#fffff0]">
+            <h1 class="text-xl text-bold md:text-2xl">Uppsss... {{ error.statusCode }}</h1>
+            <div class="py-6">
+                <p>{{ error.message }}</p>
+                <p v-if="error.statusCode == 404">
+                    Check if the search term is correct, if you think this is an error, please 
+                    <a :href="'mailto:derrickmbarani@gmail.com?subject=Error ('+error.statusCode+')&body='+error.message" target="_top">reach out</a>.
+                </p>
+            </div>
+            <button class="btn btn-green-sm" @click="handleClearError">Go Home...</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+
+    const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style scoped>
+
+</style> 
