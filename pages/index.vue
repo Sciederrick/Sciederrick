e923189b8406425e7c21d833fb16101f9a579e52
derrick <derrickmbarani@gmail.com>
2023-03-30 20:52:47 +0300

    refactor capitalize function into a composable

--- pages/index.vue
+++ pages/index.vue
@@ -96,17 +96,6 @@ const projects = ref([
     },
 ])
 
-function capitalize(input) {
-    let output = null;
-    const inputArray = input.split(' ')
-    inputArray.forEach((word, index) => {
-        if (index == 0) output = ''
-        output += word.charAt(0).toUpperCase().concat(word.substring(1))
-        if (inputArray.length != index + 1) output += " "
-    })
-    return output
-}
-
 
 </script>
 
--- pages/projects/[title].vue
+++ pages/projects/[title].vue
@@ -66,17 +66,6 @@ const images = Object.fromEntries(
 );
 templateImages.value = images
 
-function capitalize(input) {
-    let output = null;
-    const inputArray = input.split(' ')
-    inputArray.forEach((word, index) => {
-        if (index == 0) output = ''
-        output += word.charAt(0).toUpperCase().concat(word.substring(1))
-        if (inputArray.length != index + 1) output += " "
-    })
-    return output
-}
-
 const { title } = useRoute().params
 let project = ref({})
 
